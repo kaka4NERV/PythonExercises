3ab33f110d0fe28994c96b8a69f3c0e30c2d6226
kaka4NERV <ampedee@163.com>
2018-09-27 14:14:10 +0800

    upadate ex23

--- bin/ex023.py
+++ bin/ex023.py
@@ -1,1 +1,16 @@
 # 打印出如下图案（菱形）:
+'''   *
+     ***
+    *****
+   *******
+    *****
+     ***
+      *
+'''
+def pic(lines):
+    middle, lines = int(lines / 2), int(lines / 2) * 2 + 1
+    for i in range(1, lines + 1):
+        empty = abs(i - middle - 1)
+        print(' ' * empty, '*' * (2 * (middle - empty) + 1))
+line = 7 # 设置输出行数
+pic(7)
