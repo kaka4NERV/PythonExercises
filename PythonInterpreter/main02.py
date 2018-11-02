7cf2091c9d3d2ddf0972d08d7922dbdf425c555e
kaka4NERV <ampedee@163.com>
2018-11-02 12:39:58 +0800

    updates

--- PythonInterpreter/main02.py
+++ PythonInterpreter/main02.py
@@ -1,3 +1,6 @@
+import dis,sys
+
+
 class VirtualMachineError(Exception):
     pass
 
@@ -57,13 +60,95 @@ class VirtualMachine(object):
         self.frame.stack.extend(vals)
 
     def popn(self, n):
-        '''从数据栈中弹出许多的值，返回一个‘n’值的列表，最深的值在最前面P'''
+        '''从数据栈中弹出许多的值，返回一个‘n’值的列表，最深的值在最前面'''
         if n:
             ret = self.frame.stack[-n:]
             self.frame.stack[-n:] = []
             return ret
         else:
             return []
+
+    def parse_byte_and_args(self):
+        f = self.frame
+        opoffset = f.last_instruction
+        byteCode = f.code_obj.co_code[opoffset]
+        f.last_instruction += 1
+        byte_name = dis.opname[byteCode]  # dis.opname-->Sequence of operation names, indexable using the bytecode.
+        if byteCode >= dis.HAVE_ARGUMENT:  # don’t take arguments < HAVE_ARGUMENT and those which do >= HAVE_ARGUMENT.
+            # index intothe bytecode
+            arg = f.code_obj.co_code[f.last_instruction:f.last_instruction+2]
+            f.last_instruction += 2  # 下移读取指令的指针
+            arg_val = arg[0] + (arg[1] * 256)
+            if byteCode in dis.hasconst:  # 查找常量
+                arg = f.code_obj.co_consts[arg_val]
+            elif byteCode in dis.hasname:  # 查找变量
+                arg = f.code_obj.co_names[arg_val]
+            elif byteCode in dis.haslocal:  # 查找本地变量
+                arg = f.code_obj.co_varnames[arg_val]
+            elif byteCode in dis.hasjrel:  # 计算关联跳转
+                arg = f.last_instruction + arg_val
+            else:
+                arg = arg_val
+            argument = [arg]
+        else:
+            argument = []
+
+        return byte_name, argument
+
+    def dispatch(self, byte_name, argument):
+        """ Dispatch by bytename to the corresponding methods.
+               Exceptions are caught and set on the virtual machine."""
+        # When later unwinding the block stack,
+        # we need to keep track of why we are doing it.
+        why = None
+        try:
+            bytecode_fn = getattr(self, f'byte_{byte_name}', None)
+            if bytecode_fn is None:
+                if byte_name.startswith('UNARY_'):
+                    self.unaryOperator(byte_name[6:])
+                elif byte_name.startswith('BINARY_'):
+                    self.binaryOperator(byte_name[7:])
+                else:
+                    raise VirtualMachineError(
+                        f"unsupported bytecode type:{byte_name}"
+                    )
+            else:
+                why = bytecode_fn(*argument)
+        except:
+            # 处理执行该指令时遇到的异常
+            self.last_exception = sys.exc_info()[:2] + (None,)
+            why = 'exception'
+
+        return why
+
+    def run_frame(self, frame):
+        """Run a frame until it returns (somehow).
+                Exceptions are raised, the return value is returned.
+        """
+        self.push_frame(frame)
+        while True:
+            byte_name, arguments = self.parse_byte_and_args()
+
+            why = self.dispatch(byte_name, arguments)
+
+            # Deal with any block management we need to do
+            while why and frame.block_stack:
+                why = self.manage_block_stack(why)
+
+            if why:
+                break
+
+        self.pop_frame()
+
+        if why == 'exception':
+            exc, val, tb = self.last_exception
+            e = exc(val)
+            e.__traceback__ = tb
+            raise e
+
+        return self.return_value
+
+
 
 class Frame(object):
     def __init__(self, code_obj, global_names, local_names, prev_frame):
