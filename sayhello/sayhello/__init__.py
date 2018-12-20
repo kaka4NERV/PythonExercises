cf3eec4d323d16177db4164b9352992e1167179e
kaka4NERV <ampedee@163.com>
2018-12-20 21:30:01 +0800

    项目基本完成

--- sayhello/sayhello/__init__.py
+++ sayhello/sayhello/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask('sayhello')
 app.config.from_pyfile('settings.py')
@@ -7,6 +9,8 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 from sayhello import commands, views, errors
--- sayhello/sayhello/models.py
+++ sayhello/sayhello/models.py
@@ -6,4 +6,4 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(length=20))
     body = db.Column(db.String(length=200))
-    timestamp = db.Column(db.DateTime, default=datetime.now, index=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
